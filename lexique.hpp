e6197afb4ee3bacbf5112471933f650e7445f257
sophivlk <sophialoukili21@gmail.com>
2025-10-06 06:59:58 +0000

    map

--- lexique.hpp
+++ lexique.hpp
@@ -1,10 +1,11 @@
 #include <string>
+#include <map>
+
 using namespace std;
 class Lexique {
     private :
-        string mot;
-        int nbOccurences;
-
+        string nom;
+        std::map<std::string, int> lexique; // conteneur clé=mot, valeur=occurrence
     public :
         // Constructor //
         Lexique(string m, int n) : mot(m), nbOccurences(n) {}
