ea4d8acbd542da65d929412986b1b1d2dba23e66
sophivlk <sophialoukili21@gmail.com>
2025-10-06 09:51:46 +0000

    commit

---
+++ build/CMakeFiles/tp1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp1.dir/lexique.cpp.o"
+  "CMakeFiles/tp1.dir/lexique.cpp.o.d"
+  "CMakeFiles/tp1.dir/main.cpp.o"
+  "CMakeFiles/tp1.dir/main.cpp.o.d"
+  "CMakeFiles/tp1.dir/utilitaire.cpp.o"
+  "CMakeFiles/tp1.dir/utilitaire.cpp.o.d"
+  "tp1"
+  "tp1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
