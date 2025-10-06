61d5a86c78143757d5e26b88cef24a2d841b546e
Annabelle <annabelle.goter@eleves.ec-nantes.fr>
2025-10-06 08:55:35 +0200

    class lexique

--- lexique.hpp
+++ lexique.hpp
@@ -1,1 +1,17 @@
-# include "lexique.hpp"
+#include <string>
+using namespace std;
+class Lexique {
+    private :
+        string mot;
+        int nbOccurences;
+
+    public :
+        // Constructor //
+        Lexique(string m, int n) : mot(m), nbOccurences(n) {}
+
+        // Getters //
+        const string getMot() const { return mot; }
+        const int getNbOccurences() const { return nbOccurences; }
+
+        // Setters //
+};
