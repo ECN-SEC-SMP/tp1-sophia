3e568e469f3819baba7d2fadcaf09a998d50fd93
sophivlk <sophialoukili21@gmail.com>
2025-10-06 07:46:04 +0000

    construction

--- lexique.cpp
+++ lexique.cpp
@@ -1,3 +1,15 @@
-#include <string>
-#include <map>
+#include "Lexique.hpp"
+#include "utilitaire.hpp"  // pour readFileIntoString, trim_punctuation, to_lower
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
 using namespace std;
+
+Lexique::Lexique(const std::string& nomFichier)
+    : nom(nomFichier) {}
+
+Lexique::construireDepuisFichier(const std::string& nomFichier) {
+    string content;
+    readFileIntoString(std::string_view path, std::string& content);
+}
